5a53cb95c9491a05ceb36b769a8a489fbd899e29
Deajie16 <paulaandreaquitogaspar@gmail.com>
2025-12-03 19:31:17 +0800

    Revise header and navigation for Candy Hub Boutique
    Updated header content and navigation links.

--- PRLM HandsOn Mod0-2/header.php
+++ PRLM HandsOn Mod0-2/header.php
@@ -8,12 +8,15 @@
 <body>
     
 <header>
-    <h1>🍬 Candy Hub Boutique</h1>
+    <h1>Candy Hub Boutique</h1>
     <nav>
-        <a href="gaspar_variables.php">Home</a>
-        <a href="gaspar_typejuggle.php">Type Juggle</a>
-        <a href="gaspar_shorthand.php">Discounts</a>
-        <a href="gaspar_operators.php">Tax</a>
+        <a href="gaspar_variables.php">Product Overview</a>
+        <a href="gaspar_typejuggle.php">Price Conversion</a>
+        <a href="gaspar_shorthand.php">Discount Offers</a>
+        <a href="gaspar_operators.php">Tax Calculator</a>
+        <a href="functions.php">Stock Checker</a>
+
+
     </nav>
 </header>
 
